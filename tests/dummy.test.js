ff01dae3facae905e4377ccc16ea6b9bba76b5f3
Pham Duong <phamduong11@gmail.com>
2023-05-14 02:23:06 +0700

    4.5*: helper functions and unit tests, step3

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -58,3 +58,41 @@ describe('total likes', () => {
     expect(result).toBe(15);
   });
 });
+describe('Most likes of Blogs', () => {
+  test('of a most likes of blogs', () => {
+    const listWithMoreBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: '1 Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 10,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: '2 Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 6,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: '3 Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 12,
+        __v: 0,
+      },
+    ];
+    const expected = {
+      title: listWithMoreBlog[2].title, //3 Go To Statement Considered Harmful
+      author: listWithMoreBlog[2].author, //Edsger W. Dijkstra
+      likes: listWithMoreBlog[2].likes, //12
+    };
+
+    const result = listHelper.favoriteBlog(listWithMoreBlog);
+    expect(result).toEqual(expected);
+  });
+});
--- ultils/list_helper.js
+++ ultils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => sum + item.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let curLike = 0;
+  let result = {};
+  blogs.forEach((blog) => {
+    if (curLike < blog.likes) {
+      curLike = blog.likes;
+      result = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes,
+      };
+    }
+  });
+  return result;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
