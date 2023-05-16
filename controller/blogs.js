12d4c55a37f7128de599c821720eb07c15ec07c2
Pham Duong <phamduong11@gmail.com>
2023-05-17 03:17:25 +0700

    4.19: bloglist expansion, step7

--- controller/blogs.js
+++ controller/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
   const allBlog = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -19,29 +20,45 @@ blogsRouter.get('/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+
+  return null;
+};
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
-
   if (!body.title || !body.url) {
     response.status(400).end();
     return;
   }
-  const user = await User.findById(body.id);
-  if (!user) return response.status(400).end();
+  try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
+    if (!user) return response.status(400).end();
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    content: body.content,
-    url: body.url,
-    likes: body.likes || 0,
-    user: body.id,
-  });
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      content: body.content,
+      url: body.url,
+      likes: body.likes || 0,
+      user: body.id,
+    });
 
-  const saveBlog = await blog.save();
-  user.blogs = user.blogs.concat(saveBlog.id);
-  await user.save();
-  response.status(201).json(saveBlog);
+    const saveBlog = await blog.save();
+    user.blogs = user.blogs.concat(saveBlog.id);
+    await user.save();
+    response.status(201).json(saveBlog);
+  } catch (exception) {
+    next(exception);
+  }
 });
 //delete all blog
 blogsRouter.delete('/', async (request, response, next) => {
